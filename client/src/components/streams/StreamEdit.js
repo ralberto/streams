400dfceedd4d244a650bf95f96178392eb0342f7
Rui Alberto Leite Goncalves <ralberto@hp-rui.lan>
2020-01-03 16:41:31 +0000

    Added ability to create adn edit streams

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
-};
+import { connect } from 'react-redux';
+import {fetchStream, editStream} from '../../actions';
+import StreamForm from './StreamForm';
 
-export default StreamEdit; 
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    onSubmit = (formValues) =>  {
+        this.props.editStream(this.props.match.params.id, formValues);
+    }
+
+    render() {
+        if(!this.props.stream)  {
+            return <div>Loading...</div>
+        } else {
+            //initial values contains an hash with {name: ... , description: ... }
+            //values from this keys will be used on the StreamForm/Field component with the
+            //same "name" attribute.
+            const {title, description} = this.props.stream;
+            return (
+                //initialValues={this.props.stream} - Will pass all the stream props, including 
+                //userId and stream ID... depending on the server setup it might consideder that
+                //we also want to update the userId and streamId
+
+                //So we must pass only the name and descriptiom
+                //Option 1 - {name: this.props.stream.name, description: this.props.stream.description }
+                //Option 2 -
+                <StreamForm onSubmit={this.onSubmit} initialValues={{title, description}}/>
+            )
+        }
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit); 
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 import {fetchStreams} from '../../actions'
 
@@ -10,10 +11,26 @@ class StreamList extends React.Component {
         this.props.fetchStreams()
     }
 
+    renderAdmin = (stream) => {
+        if(stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>
+                        Edit
+                    </Link>
+                    <Link className="ui button negative" to={`/streams/delete/${stream.id}`}>
+                        Delete
+                    </Link>
+                </div>
+            );
+        }
+    }
+
     renderList = () => {
         return this.props.streams.map((stream) => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle  aligned icon camera"></i>
                     <div className="content">
                         {stream.title}
@@ -26,6 +43,16 @@ class StreamList extends React.Component {
         )
     }
 
+    renderCreate = () => {
+        if(this.props.isSignedIn) {
+            return (
+                <div style={{textAlign: 'right'}}>
+                    <Link to="/streams/new" className="ui button primary">Create steam</Link>
+                </div>
+            )
+        }
+    }
+
     render() {
 
         return (
@@ -34,6 +61,7 @@ class StreamList extends React.Component {
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         )
     }
@@ -42,7 +70,9 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     return {
         //Converting Object to Array
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
     
 }
